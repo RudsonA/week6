fcea06f5ce4a9b9ed55ddeccc4996b9e1ae5aa68
RudsonA <rudson.augustin@yahoo.com>
2019-02-20 15:40:38 -0500

    working on take home work

---
+++ toDoApp.php
@@ -0,0 +1,49 @@
+
+<form action="" method="post">
+    <input type="text" name="item" placeholder="Enter to do item"> 
+    <input type="submit" value="Add">
+</form>
+
+<?php
+
+
+if ($_POST) {
+    //Validating string 
+    $item = $_POST["item"];
+        function validateItem($item) {
+            // $item = trim($item);
+            // $item = stripslashes($item);
+            // $item = htmlspecialchars($item);
+            $item = trim(stripslashes(htmlspecialchars($item)));
+            return $item;
+        }
+        validateItem($item); 
+        
+        //Inserting text field value uinto database
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "todoapp";
+        
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "INSERT INTO list (task, progress)
+            VALUES ('$item', 'Incomplete')";
+            // use exec() because no results are returned
+            $conn->exec($sql);
+            echo "New record created successfully";
+            }
+        catch(PDOException $e)
+            {
+            echo $sql . "<br>" . $e->getMessage();
+            }
+        
+        $conn = null;
+    }
+
+
+include "toDoApp-current.php";
+
+?>
