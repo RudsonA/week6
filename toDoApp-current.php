cd015a9148d6e0d8a842aa7f309b38d064aa2330
RudsonA <rudson.augustin@yahoo.com>
2019-02-20 16:03:12 -0500

    work at home

--- toDoApp-current.php
+++ toDoApp-current.php
@@ -5,15 +5,24 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 
+echo "<table>";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=toDoApp", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
+    $stmt = $conn->prepare("SELECT id, task, progress FROM list"); 
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $appList = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    
+    var_dump($appList);
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
+
 
 ?>
