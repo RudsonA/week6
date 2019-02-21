ed4e4ab06fccf020294bbd1798f14736dce9a186
RudsonA <rudson.augustin@yahoo.com>
2019-02-21 10:09:20 -0500

    working on delete in current

--- toDoApp/toDoApp-current.php
+++ toDoApp/toDoApp-current.php
@@ -14,9 +14,37 @@ try {
     $stmt->execute();
 
     // set the resulting array to associative
-    $appList = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    $appList = $stmt->fetchAll();
     
-    var_dump($appList);
+    foreach ($appList as $a) {
+        echo "<tr>";
+        echo "<td>" . $a['task'] . "</td>";
+        echo  "<td>" . "<form method='post' action=''>" . "<input hidden value=".$a['id']."> <input type='submit' value='DONE'> </form>" . "</td>"; 
+        echo "</tr>";
+    }
+
+    if ($_POST) {
+        $servername = "localhost"; 
+        $username = "root";
+        $password = "";
+        $dbname = "todoapp"; 
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+               // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // sql to delete a record
+    $sql = "DELETE FROM MyGuests WHERE id=3";
+
+        }
+        catch (PDOExeception $e) {
+            echo $sql . "<br>" . $e->getMessage(); 
+        }
+
+        $conn = null; 
+    }
 }
 catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
