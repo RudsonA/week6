349a8b5ce0e432a8cda3eb69993a788b604070ef
RudsonA <rudson.augustin@yahoo.com>
2019-02-21 13:07:21 -0500

    app working, cleaning up code now

--- toDoApp/toDoApp-current.php
+++ toDoApp/toDoApp-current.php
@@ -1,4 +1,7 @@
 
+
+<?php include "connect.php"; ?>
+
 <?php
 
 $servername = "localhost";
@@ -10,26 +13,20 @@ try {
     $conn = new PDO("mysql:host=$servername;dbname=toDoApp", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT id, task, progress FROM list"); 
-    $stmt->execute();
-
-    // set the resulting array to associative
-    $stmt->setFetchMode(PDO::FETCH_ASSOC); 
-    $appList = $stmt->fetchAll();
     
-    foreach ($appList as $a) {
-        echo "<tr>";
-        echo "<td>" . $a['task'] . "</td>";
-        echo  "<td> <form method='post' action=''> <input hidden name='del' value='" . $a['id'] . "'> <input type='submit' value='DONE'> </form>" . "</td>"; 
-        echo "</tr>";
-    }
+    // foreach ($appList as $a) {
+    //     echo "<tr>";
+    //     echo "<td>" . $a['task'] . "</td>";
+    //     echo  "<td> <form method='post' action=''> <input hidden name='del' value='" . $a['id'] . "'> <input type='submit' value='DONE'> </form>" . "</td>"; 
+    //     echo "</tr>";
+    // }
     
-    $conn = null; 
+     
     }
     catch(PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
-    $conn = null;
+    
     // echo "</table>";
     
 
@@ -55,7 +52,7 @@ try {
             echo "Record deleted successfully";
 
             //Update page function
-            header("Refresh:0");
+            // header("Refresh:0");
             
         }
         catch (PDOExeception $e) {
@@ -63,12 +60,19 @@ try {
         }
     }
     
-    // foreach ($appList as $a) {
-    //     echo "<tr>";
-    //     echo "<td>" . $a['task'] . "</td>";
-    //     echo  "<td> <form method='post' action=''> <input hidden name='del' value='" . $a['id'] . "'> <input type='submit' value='DONE'> </form>" . "</td>"; 
-    //     echo "</tr>";
-    // }
+    $stmt = $conn->prepare("SELECT id, task, progress FROM list"); 
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    $appList = $stmt->fetchAll();
+
+    foreach ($appList as $a) {
+        echo "<tr>";
+        echo "<td>" . $a['task'] . "</td>";
+        echo  "<td> <form method='post' action=''> <input hidden name='del' value='" . $a['id'] . "'> <input type='submit' value='DONE'> </form>" . "</td>"; 
+        echo "</tr>";
+    }
     
     echo "</table>";
 
